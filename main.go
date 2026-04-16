5de3a6c25e058ad65cdda83567aaf7d7ae692603
kqtagiri <mezko321@gmail.com>
2026-04-16 19:25:43 +0300

    add goroutines patterns

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"io"
@@ -8,6 +9,9 @@ import (
 	"net/http"
 	"net/url"
 	"os"
+	"os/signal"
+	"sync"
+	"syscall"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
@@ -68,29 +72,60 @@ func main() {
 		return
 	}
 
+	ctx, cancel := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer cancel()
+
 	u := tgbotapi.NewUpdate(0)
-	u.Timeout = 10
+	u.Timeout = 30
 	updates := bot.GetUpdatesChan(u)
+	tasks := make(chan tgbotapi.Update, 100)
+	const workers = 3
+	wg := sync.WaitGroup{}
 
-	for update := range updates {
+	for i := 0; i < workers; i++ {
 
-		if update.Message == nil {
-			continue
-		}
+		wg.Add(1)
+		go func() {
 
-		slog.Info(fmt.Sprintf("Get new message: %s\nfrom %s", update.Message.Text, update.Message.From))
+			defer wg.Done()
+			for update := range tasks {
 
-		if update.Message.Command() == "start" {
-			text := fmt.Sprintf("Привет, %s! Я бот, который выдает прогноз погоды городов. Ты можешь написать название города, а я выдам тебе актуальную температуру в данном городе!", update.Message.From.FirstName)
-			msg := tgbotapi.NewMessage(update.Message.Chat.ID, text)
-			bot.Send(msg)
-			continue
-		}
+				if update.Message != nil {
+					slog.Info(fmt.Sprintf("Get new message: %s\nfrom %s", update.Message.Text, update.Message.From))
+					var text string
+					if update.Message.Command() == "start" {
+						text = fmt.Sprintf("Привет, %s! Я бот, который выдает прогноз погоды городов. Ты можешь написать название города, а я выдам тебе актуальную температуру в данном городе!", update.Message.From.FirstName)
+					} else {
+						text = GetTemp(update.Message.Text, w_api)
+					}
+					msg := tgbotapi.NewMessage(update.Message.Chat.ID, text)
+					bot.Send(msg)
 
-		text := GetTemp(update.Message.Text, w_api)
-		msg := tgbotapi.NewMessage(update.Message.Chat.ID, text)
-		bot.Send(msg)
+				}
+
+			}
+
+		}()
 
 	}
+	fmt.Println("Start check updates")
+	go func() {
+		for update := range updates {
+
+			select {
+			case <-ctx.Done():
+				break
+			default:
+				tasks <- update
+			}
+
+		}
+	}()
+
+	<-ctx.Done()
+	close(tasks)
+	fmt.Println("wait until workers are working")
+	wg.Wait()
+	fmt.Println("GG")
 
 }
